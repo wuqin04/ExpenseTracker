e0646a6c074e2290807393f5398bf6c0d4b6789e
Wuqin04 <wuqin419@gmail.com>
2025-08-30 17:03:19 +0800

    Added custom simple debugger

--- src/functions/logic.py
+++ src/functions/logic.py
@@ -1,11 +1,11 @@
 import customtkinter as ctk
 import ui.widget as ui
-from utils.helper import Direction
+from utils.helper import Direction, Debug
 
 BALANCE_AMOUNT = 1000
 
 def create_new_expense(object, main):
-    print("Adding Expense...")
+    Debug("Adding Expense...")
 
     object.configure(state="disabled")
 
@@ -13,4 +13,4 @@ def create_new_expense(object, main):
     get_user_expense.grid(row=5, column=0, sticky=Direction.UP)
 
 def add_expense():
-    print("Added Expenses")
+    Debug("Added Expenses")
--- src/utils/helper.py
+++ src/utils/helper.py
@@ -10,3 +10,15 @@ class Direction(StrEnum):
     UP_RIGHT    = "NE"
     DOWN_LEFT   = "SW"
     DOWN_RIGHT  = "SE"
+
+def Debug(str):
+    len_escape = 12 + len(str)
+    for i in range(len_escape):
+        print("-", end="")
+
+    print(f"\n[Debugger]: {str}")
+
+    for i in range(len_escape):
+        print("-", end="")
+
+    print()
